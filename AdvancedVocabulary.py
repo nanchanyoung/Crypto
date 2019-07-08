943fe36cf8207421f9ae098121c93d3c2a1d3c20
entransCykim <46589905+entransCykim@users.noreply.github.com>
2019-07-08 18:57:05 +0900

    단어장 과제 진행중

--- AdvancedVocabulary.py
+++ AdvancedVocabulary.py
@@ -18,12 +18,25 @@ for word in in_file:
 
     voca[korean_word] = english_word
 
+    '''
     guess = input('%s: ' % korean_word)
 
-    if guess == voca[english_word]:
+    if guess == voca[korean_word]:
         print('맞았습니다! \n')
     else:
         print('아쉽습니다. 정답은 %s입니다. \n' % voca[english_word])
+    '''
+
+keys = list(voca.keys())
+print(keys)
+'''
+while True:
+    index = randint(0, len(keys) - 1)
+    korean_word = keys[index]
+    print(korean_word)
+    english_word = voca[korean_word]
+'''
+
 
 #파일 닫기
 in_file.close()
