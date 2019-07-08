006cb6f9e90216d5b5a3de6a1bb977bb47c8b3cb
entransCykim <46589905+entransCykim@users.noreply.github.com>
2019-07-08 15:19:44 +0900

    완료

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,20 @@
-print('1')
+def is_palindrome(word):
+    #print(len(word))
+    true_count = 0
+
+    for i in range(int(len(word) / 2)):
+        #print('앞' + word[i])
+        #print('뒤' + word[len(word) - i - 1])
+
+        if word[i] !=  word[len(word) - i - 1]:
+            true_count = true_count + 1
+            return_str = False
+        else:
+            return_str = True
+    return  return_str
+
+print(is_palindrome("racecar"))
+print(is_palindrome("stars"))
+print(is_palindrome("토마토"))
+print(is_palindrome("kayak"))
+print(is_palindrome("hello"))
